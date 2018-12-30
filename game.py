66790104c36017a386d2ac13259375c6dd0f09ae
penlin <penlin0217@gmail.com>
2018-12-30 20:14:00 +0800

    [I] init commit , a ver 0.1 snake only have head

---
+++ game.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+import abc
+import enum
+
+class GameStatus(enum.Enum):
+    EGameNotInit = 'NotInit',
+    EGameReady = 'Ready',
+    EGameStart = 'Start, press \'p\' to pasue',
+    EGamePause = 'Pause, press \'p\' to resume',
+    EGameOver = 'Game Over!',
+    EGameVictory = 'Congradulation! You Win',
+    EGameStop = 'Stop',
+
+
+class Game(abc.ABC):
+    size = (250, 250)
+    status = GameStatus.EGameNotInit
+    frame_rate = 50
+    scl = 5
+    _status_bar_height = 5
+    _title = 'A New Game'
+    _time = 0
+
+    def __init__(self, title, size, frame_rate=50, scale=50):
+        self._title = title
+        self.size = size
+        self.frame_rate=frame_rate
+        self.scl = scale
+        self.status = GameStatus.EGameReady
+
+    def start(self, game_inst):
+        self.status = GameStatus.EGameStart
+        delay = int(1000/self.frame_rate)
+        cv2.namedWindow(self._title)
+        cv2.moveWindow(self._title, 200, 0)
+        self.w = int(self.size[0] * self.scl)
+        self.h = int((self.size[1] + self._status_bar_height) * self.scl)
+        canvas = np.zeros((self.h, self.w, 3), dtype=np.int8)
+        while self.status != GameStatus.EGameStop:
+            if self.status == GameStatus.EGameStart:
+                self._time = self._time + 1
+                self.update(game_inst)
+                if self.status in [GameStatus.EGameOver, GameStatus.EGameVictory]:
+                    break
+            self.show(canvas, game_inst)
+            key = cv2.waitKey(delay)                
+            if key == ord('p'):
+                if self.status == GameStatus.EGamePause:
+                    self.status = GameStatus.EGameStart
+                elif self.status == GameStatus.EGameStart:
+                    self.status = GameStatus.EGamePause
+            elif key in [ord('q'), 27]:
+                self.status = GameStatus.EGameStop
+            elif (key & 0xFF) != 0xFF:
+                self.onKeyPressed(key, game_inst)
+
+        self.show(canvas, game_inst)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def show(self, canvas, game_inst):
+        self.drawMain(canvas)
+        self.draw(canvas, game_inst)
+        cv2.imshow(self._title, canvas)
+
+    def drawMain(self, canvas):
+        win_w = int(self.size[0] * self.scl)
+        win_h = int(self.size[1] * self.scl)
+        cv2.rectangle(canvas, (0, 0), (win_w, win_h), (0, 0, 0), -1)
+        cv2.rectangle(canvas, (0, win_h), (win_w, self.h), (30, 30, 30), -1)
+        if self.status != GameStatus.EGameStop:
+            status = '[{}]: {}'.format(self._time, self.status)
+        else:
+            status = '[{}]: Game Ended, press any key to exit ...'.format(self._time)
+        cv2.putText(canvas, status, (self.scl * 5, int((win_h + self.h)/2)), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+
+    @abc.abstractmethod
+    def update(self, game_inst):
+        pass
+
+    @abc.abstractmethod
+    def draw(self, canvas, game_inst):
+        pass
+
+    @abc.abstractmethod
+    def onKeyPressed(self, key, game_inst):
+        pass
---
+++ snake_game.py
@@ -0,0 +1,79 @@
+from game import GameStatus, Game
+import cv2
+import random as rand
+
+def dist(p1, p2):
+    return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
+
+def same_pos(p1, p2):
+    return dist(p1, p2) < 1
+
+class Snake():
+    def __init__(self, x=rand.randint(0, 10), y=rand.randint(0, 10)):
+        self.x = x
+        self.y = y
+        self.xspeed = 1
+        self.yspeed = 0
+    
+    def dir(self, x, y):
+        self.xspeed = x
+        self.yspeed = y
+    
+    def update(self, size):
+        self.x = self.x + self.xspeed
+        self.y = self.y + self.yspeed
+        if self.x < 0:
+            self.x = 0
+        if self.y < 0:
+            self.y = 0
+        if self.x >= size[0]:
+            self.x = size[0]-1
+        if self.y >= size[1]:
+            self.y = size[1]-1
+
+    def eat(self, x, y):
+        return same_pos((self.x, self.y), (x, y))
+
+
+class SnakeGame(Game):
+    candy = (0, 0)
+    score = -1
+
+    def __init__(self, size=(50, 50), frame_rate=60, scale=10):
+        super().__init__('Snake Game', size, frame_rate=frame_rate, scale=scale)
+        self.create_candy()        
+
+    def create_candy(self):
+        self.candy = (rand.randint(0, self.size[0]-1), rand.randint(0, self.size[1]-1))
+        self.score += 1
+
+    def update(self, game_inst):
+        game_inst.update(self.size)
+        if game_inst.eat(self.candy[0], self.candy[1]):
+            self.create_candy()
+
+    def draw(self, canvas, game_inst):
+        x, y = int(game_inst.x * self.scl), int(game_inst.y * self.scl)
+        cv2.rectangle(canvas, (x, y), (x + self.scl, y + self.scl), (200, 210, 255), -1)
+        cx, cy = int(self.candy[0]* self.scl), int(self.candy[1]* self.scl)
+        cv2.rectangle(canvas, (cx, cy), (cx + self.scl, cy + self.scl), (0, 0, 255), -1)
+        win_w = int(self.size[0] * self.scl)
+        win_h = int((self.size[1] * self.scl + self.h)/2)
+        cv2.putText(canvas, 'Score:{}'.format(self.score), (win_w - 100, win_h), cv2.FONT_HERSHEY_PLAIN, 1, (200, 200, 255), 1)
+
+    def onKeyPressed(self, key, game_inst):
+        print('{} pressed'.format(key))
+        if key == ord('i'):
+            game_inst.dir(0, -1)
+        elif key == ord('k'):
+            game_inst.dir(0, 1)
+        elif key == ord('j'):
+            game_inst.dir(-1, 0)
+        elif key == ord('l'):
+            game_inst.dir(1, 0)
+
+
+if __name__ == '__main__':
+    game = SnakeGame(frame_rate=10)
+    obj = Snake()
+    game.start(obj)
