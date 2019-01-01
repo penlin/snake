270e96ec8e3277af386a40bec679a0d5fdcc37f9
penlin <penlin0217@gmail.com>
2019-01-01 16:07:51 +0800

    [M] support save record and resume

--- snake_game.py
+++ snake_game.py
@@ -1,5 +1,8 @@
 from game import GameStatus, Game
 import cv2
+import sys
+import argparse
+import pickle
 import random as rand
 
 def dist(p1, p2):
@@ -9,66 +12,75 @@ def same_pos(p1, p2):
     return dist(p1, p2) < 1
 
 class Snake():
-    def __init__(self, x=rand.randint(0, 10), y=rand.randint(0, 10)):
-        self.x = x
-        self.y = y
+    def __init__(self, size, x=-1, y=-1):
+        self.size = size
+        if x < 0 or x > (self.size[0]-2):
+            self.x  = rand.randint(0, self.size[0]-2)
+        else:
+            self.x = x
+        if y < 0 or y > (self.size[1]-2):
+            self.y = rand.randint(0, self.size[1]-2)
+        else:
+            self.y = y
         self.tail = []
         self.xspeed = 1
         self.yspeed = 0
-    
+        self.create_candy()
+
     def dir(self, x, y):
         if self.xspeed == -x and self.yspeed == -y:
             return 
         self.xspeed = x
         self.yspeed = y
     
-    def update(self, size):
+    def update(self):
         if len(self.tail) > 0:
             self.tail = self.tail[1:] + [(self.x, self.y)]
         self.x = self.x + self.xspeed
         self.y = self.y + self.yspeed
-        if self.x < 0 or self.y < 0 or self.x >= size[0] or self.y >= size[1]:
+        if self.x < 0 or self.y < 0 or self.x >= self.size[0] or self.y >= self.size[1]:
             return False
         if (self.x, self.y) in self.tail:
             return False
         return True
 
-    def eat(self, x, y):
-        if same_pos((self.x, self.y), (x, y)):
+    def create_candy(self):
+        num = rand.randint(0, self.size[0] * self.size[1]-1)
+        while num != (self.x * self.size[0] + self.y):
+            num = rand.randint(0, self.size[0] * self.size[1]-1)
+        self.candy = (num % self.size[0], int(num/ self.size[0]))
+
+    def eat(self):
+        if same_pos((self.x, self.y), self.candy):
             self.tail.append((self.x, self.y))
+            self.create_candy()
             return True
         return False
 
 class SnakeGame(Game):
-    candy = (0, 0)
-    score = -1
-
-    def __init__(self, size=(50, 50), frame_rate=60, scale=10):
-        super().__init__('Snake Game', size, frame_rate=frame_rate, scale=scale)
-        self.create_candy()        
-
-    def create_candy(self):
-        self.candy = (rand.randint(0, self.size[0]-1), rand.randint(0, self.size[1]-1))
-        self.score += 1
+    def __init__(self, size=(50, 50), frame_rate=60, scale=10, controller=None):
+        super().__init__('Snake Game', size, frame_rate=frame_rate, scale=scale, controller=controller)
 
     def update(self, game_inst):
-        if not game_inst.update(self.size):
+        if not game_inst.update():
             self.status = GameStatus.EGameOver
             return
-        if game_inst.eat(self.candy[0], self.candy[1]):
-            self.create_candy()
+        game_inst.eat()
 
     def draw(self, canvas, game_inst):
         for pt in game_inst.tail:
             self.rect(canvas, pt[0], pt[1], (150, 160, 200))
         self.rect(canvas, game_inst.x , game_inst.y, (200, 210, 255))
-        self.rect(canvas, self.candy[0], self.candy[1], (0, 0, 255))
+        self.rect(canvas, game_inst.candy[0], game_inst.candy[1], (0, 0, 255))
         win_w = int(self.size[0] * self.scl)
         win_h = int((self.size[1] * self.scl + self.h)/2)
-        cv2.putText(canvas, 'Score:{}'.format(self.score), (win_w - 100, win_h), cv2.FONT_HERSHEY_PLAIN, 1, (200, 200, 255), 1)
+        cv2.putText(canvas, 'Score:{}'.format(len(game_inst.tail)), (win_w - 100, win_h), cv2.FONT_HERSHEY_PLAIN, 1, (200, 200, 255), 1)
 
     def onKeyPressed(self, key, game_inst):
-        if key == ord('i'):
+        if key == ord('s'):
+            with open('record.tmp', 'wb') as ftmp:
+                pickle.dump(game_inst, ftmp)
+        elif key == ord('i'):
             game_inst.dir(0, -1)
         elif key == ord('k'):
             game_inst.dir(0, 1)
@@ -79,6 +91,18 @@ class SnakeGame(Game):
 
 
 if __name__ == '__main__':
-    game = SnakeGame(frame_rate=10)
-    obj = Snake()
+    parser = argparse.ArgumentParser(description='Play Snake Game')
+    parser.add_argument('--resume', dest='record',
+                         help='resume from the specified saved record')
+    parser.add_argument('--fps', default=10, type=int, help='frame rate')
+    parser.add_argument('--size', default='50x50', help='game board size')
+    parser.add_argument('--scale', default=10, type=int, help='grid size')
+    args = parser.parse_args()
+    size = tuple(map(int, args.size.split('x')))
+    game = SnakeGame(size=size, frame_rate=args.fps, scale=args.scale)
+    if args.record:
+        with open(args.record, 'rb') as fin:
+            obj = pickle.load(fin)
+    else:
+        obj = Snake(game.size)
     game.start(obj)
