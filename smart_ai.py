fb02649dc6d27e9c6a66559cea27dc2cce69aa27
penlin <penlin0217@gmail.com>
2019-01-03 22:43:28 +0800

    [M] smarter AI to prevent die when no tail

--- smart_ai.py
+++ smart_ai.py
@@ -11,16 +11,17 @@ def get_smart_ai(size):
         candidate = {ord('j'): (-1, 0), ord('i'): (0, -1), ord('l'): (1, 0), ord('k'): (0, 1)}
         valid_candidate = {}
         for key, speed in candidate.items():
+            if speed[0] == -inst.xspeed and speed[1] == -inst.yspeed:
+                continue
             pos = (inst.x + speed[0], inst.y + speed[1])
             if 0 <= pos[0] < size[0] and 0 <= pos[1] < size[1]:
                 if len(inst.tail) <= 1 or pos not in inst.tail[1:]:
                     valid_candidate[key] = pos
-        min_val = dist((inst.x, inst.y), inst.candy)
+        min_val = size[0] * size[1]
         min_key = 0xFF
         for key, pos in valid_candidate.items():
             val = dist(pos, inst.candy)
             if val < min_val:
-                print('found {} -> dist={}'.format(speed, val))
                 min_val = val
                 min_key = key
         if min_key == 0xFF:
